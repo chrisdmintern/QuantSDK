32be05c98112f96c64ba2665516dc34f25140fa7
chrisdmintern <cm@blocksize-capital.com>
2021-02-12 18:33:01 +0100

    Response Modifications

--- quant_sdk_lite/quantsdklite.py
+++ quant_sdk_lite/quantsdklite.py
@@ -1,7 +1,6 @@
 import requests
 import datetime
 import pandas as pd
-import re
 from typing import Union, List
 
 
@@ -12,40 +11,42 @@ class BlockSize:
 
     def get_orderbook_data(self, exchanges: Union[str, List[str]], base: str, quote: str, depth: int = 1) -> dict:
 
-        if type(exchanges) == list:
-            exchanges = ', '.join(exchanges)
+        if exchanges is None:
+            pass
+        else:
+            if type(exchanges) == list:
+                exchanges = list(map(lambda exchange: exchange.upper(), exchanges))
+                exchanges = ','.join(exchanges)
+            if type(exchanges) == str:
+                exchanges = exchanges.replace(' ', '')
 
-        try:
-            pair = base + quote
-            response = requests.get(f"https://api.blocksize.capital/v1/data/orderbook?exchanges={exchanges}"
-                                    f"&ticker={pair}&limit={depth}", headers={"x-api-key": self.token})
+        if base is None or quote is None:
+            raise ValueError('Either pair or base and quote need to be specified')
 
-            return response.json()
-
-        except Exception as ex:
-            print(ex)
+        pair = base + quote
+        response = requests.get(f"https://api.blocksize.capital/v1/data/orderbook?exchanges={exchanges}"
+                                f"&ticker={pair}&limit={depth}", headers={"x-api-key": self.token})
+        return response.json()
 
     def get_vwap(self, base: str, quote: str, interval: str):
 
-        try:
-            pair = base + quote
-            response = requests.get(
-                f"https://api.blocksize.capital/v1/data/vwap/latest/{pair}/{self.converter(self.converter(interval))}",
-                headers={"x-api-key": self.token})
-            return response.json()
-        except Exception as ex:
-            print(ex)
+        if type(interval) == str:
+            interval = self.converter(interval)
+        pair = base + quote
+        response = requests.get(
+            f"https://api.blocksize.capital/v1/data/vwap/latest/{pair}/{self.converter(self.converter(interval))}",
+            headers={"x-api-key": self.token})
+        return response.json()
 
     def get_ohlc(self, base: str, quote: str, interval: str):
 
-        try:
-            pair = base + quote
-            response = requests.get(
-                f"https://api.blocksize.capital/v1/data/ohlc/latest/{pair}/{self.converter(self.converter(interval))}",
-                headers={"x-api-key": self.token})
-            return response.json()
-        except Exception as ex:
-            print(ex)
+        if type(interval) == str:
+            interval = self.converter(interval)
+        pair = base + quote
+        response = requests.get(
+            f"https://api.blocksize.capital/v1/data/ohlc/latest/{pair}/{self.converter(self.converter(interval))}",
+            headers={"x-api-key": self.token})
+        return response.json()
 
     def get_historical_vwap(
             self,
@@ -65,21 +66,17 @@ class BlockSize:
         :return:
         """
 
-        try:
-
-            pair = base + quote
-            response = requests.get(f"https://api.blocksize.capital/v1/data/vwap/historic/{pair}/"
-                                    f"{self.converter(interval)}?from={start_date}&to={end_date}",
-                                    headers={"x-api-key": self.token})
-            df = pd.DataFrame(response.json())
-            if df.empty:
-                return df
-            df.rename(columns={'timestamp': 'Time', 'price': 'Price', 'volume': 'Volume'}, inplace=True)
-            df['Time'] = pd.to_datetime(df['Time'], unit='s')
-            df.set_index('Time', inplace=True)
+        pair = base + quote
+        response = requests.get(f"https://api.blocksize.capital/v1/data/vwap/historic/{pair}/"
+                                f"{self.converter(interval)}?from={start_date}&to={end_date}",
+                                headers={"x-api-key": self.token})
+        df = pd.DataFrame(response.json())
+        if df.empty:
             return df
-        except Exception as ex:
-            print(ex)
+        df.rename(columns={'timestamp': 'Time', 'price': 'Price', 'volume': 'Volume'}, inplace=True)
+        df['Time'] = pd.to_datetime(df['Time'], unit='s')
+        df.set_index('Time', inplace=True)
+        return df
 
     def get_historic_ohlc(
             self,
@@ -99,24 +96,21 @@ class BlockSize:
         :return:
         """
 
-        try:
-            pair = base + quote
-            response = requests.get(f"https://api.blocksize.capital/v1/data/ohlc/historic/"
-                                    f"{pair}/{self.converter(interval)}?from={start_date}&to={end_date}",
-                                    headers={"x-api-key": self.token})
-            df = pd.DataFrame(response.json())
-            if df.empty:
-                return df
-            df.rename(columns={'timestamp': 'Time',
-                               'open': 'Open',
-                               'high': 'High',
-                               'low': 'Low',
-                               'close': 'Close'}, inplace=True)
-            df.set_index('Time', inplace=True)
-
+        pair = base + quote
+        response = requests.get(f"https://api.blocksize.capital/v1/data/ohlc/historic/"
+                                f"{pair}/{self.converter(interval)}?from={start_date}&to={end_date}",
+                                headers={"x-api-key": self.token})
+        df = pd.DataFrame(response.json())
+        if df.empty:
             return df
-        except Exception as ex:
-            print(ex)
+        df.rename(columns={'timestamp': 'Time',
+                           'open': 'Open',
+                           'high': 'High',
+                           'low': 'Low',
+                           'close': 'Close'}, inplace=True)
+        df.set_index('Time', inplace=True)
+
+        return df
 
     def post_simulated_order(
             self,
@@ -125,34 +119,32 @@ class BlockSize:
             direction: str,
             quantity: Union[str, float, int],
             exchanges: Union[str, List[str]] = None,
-            unlimited_funds: bool = False) -> dict:
+            unlimited_funds: bool = False,
+            disable_logging: bool = False) -> dict:
 
-        try:
+        if exchanges is None:
+            pass
+        else:
+            if type(exchanges) == list:
+                exchanges = list(map(lambda exchange: exchange.upper(), exchanges))
+                exchanges = ','.join(exchanges)
+            elif type(exchanges) == str:
+                exchanges = exchanges.upper()
 
-            if exchanges is None:
-                pass
+        params = {
+            'BaseCurrency': base.upper(),
+            'QuoteCurrency': quote.upper(),
+            'Quantity': quantity,
+            'Direction': direction.upper(),
+            'Type': 'Market',
+            'ExchangeList': exchanges,
+            'Unlimited': unlimited_funds,
+            'DisableLogging': disable_logging,
+        }
 
-            else:
-                if type(exchanges) == list:
-                    exchanges = list(map(lambda exchange: exchange.upper(), exchanges))
-                elif type(exchanges) == str:
-                    exchanges = exchanges.upper()
-
-            params = {
-                'BaseCurrency': base.upper(),
-                'QuoteCurrency': quote.upper(),
-                'Quantity': quantity,
-                'Direction': direction.upper(),
-                'Type': 'Market',
-                'ExchangeList': exchanges,
-                'Unlimited': unlimited_funds,
-            }
-
-            response = requests.post("https://api.blocksize.capital/v1/trading/orders/simulated", data=params,
-                                     headers={"x-api-key": self.token})
-            return response.json()
-        except Exception as ex:
-            print(ex)
+        response = requests.post("https://api.blocksize.capital/v1/trading/orders/simulated", data=params,
+                                 headers={"x-api-key": self.token})
+        return response.json()
 
     def post_market_order(
             self,
@@ -162,77 +154,49 @@ class BlockSize:
             quantity: Union[str, float, int],
             exchanges: Union[str, List[str]] = None) -> dict:
 
-        try:
-            if exchanges is None:
-                pass
+        if exchanges is None:
+            pass
+        else:
+            if type(exchanges) == list:
+                exchanges = list(map(lambda exchange: exchange.upper(), exchanges))
+                exchanges = ','.join(exchanges)
+            elif type(exchanges) == str:
+                exchanges = exchanges.upper()
 
-            else:
-                if type(exchanges) == list:
-                    exchanges = list(map(lambda exchange: exchange.upper(), exchanges))
-                elif type(exchanges) == str:
-                    exchanges = exchanges.upper()
+        params = {
+            'BaseCurrency': base.upper(),
+            'QuoteCurrency': quote.upper(),
+            'Quantity': quantity,
+            'Direction': direction,
+            'Type': 'MARKET',
+            'ExchangeList': exchanges,
+        }
 
-            params = {
-                'BaseCurrency': base.upper(),
-                'QuoteCurrency': quote.upper(),
-                'Quantity': quantity,
-                'Direction': direction,
-                'Type': 'MARKET',
-                'ExchangeList': exchanges,
-            }
+        response = requests.post("https://api.blocksize.capital/v1/trading/orders?", data=params,
+                                 headers={"x-api-key": self.token})
 
-            response = requests.post("https://api.blocksize.capital/v1/trading/orders?", data=params,
-                                     headers={"x-api-key": self.token})
-
-            return response.json()
-        except Exception as ex:
-            print(ex)
+        return response.json()
 
     def order_status(self, order_id: str):
-
-        try:
-            response = requests.get(f"https://api.blocksize.capital/v1/trading/orders/id/{order_id}",
-                                    headers={"x-api-key": self.token})
-            return response.json()
-
-        except Exception as ex:
-            print(ex)
+        response = requests.get(f"https://api.blocksize.capital/v1/trading/orders/id/{order_id}",
+                                headers={"x-api-key": self.token})
+        return response.json()
 
     def order_logs(self, order_id: str):
-
-        try:
-            response = requests.get(f'https://api.blocksize.capital/v1/trading/orders/id/{order_id}/logs',
-                                    headers={"x-api-key": self.token})
-            return response.json()
-        except Exception as ex:
-            print(ex)
+        response = requests.get(f'https://api.blocksize.capital/v1/trading/orders/id/{order_id}/logs',
+                                headers={"x-api-key": self.token})
+        return response.json()
 
     def get_exchange_balances(self):
-
-        try:
-            response = requests.get('https://api.blocksize.capital/v1/positions/exchanges',
-                                    headers={"x-api-key": self.token})
-            return response.json()
-        except Exception as ex:
-            print(ex)
+        response = requests.get('https://api.blocksize.capital/v1/positions/exchanges',
+                                headers={"x-api-key": self.token})
+        return response.json()
 
     @staticmethod
     def converter(interval_string: str) -> str:
-
-        try:
-            # allows all seconds and interval in respective time-unit
-            # allowed: ex. 10m, 600s, 60s, 1m, 2h, 7200s
-            # not allowed: ex. 120m, 360m
-            # broad sense check of parameters
-
-            assert re.match(r'^[1-5][0-9]?[mh]$|^60[m]$|^[0-9]{1,5}[s]$', interval_string), 'invalid interval pattern'
-
-            if interval_string[-1] == 's':
-                return interval_string
-            elif interval_string[-1] == 'm':
-                return str(f'{int(interval_string[:-1]) * 60}s')
-            elif interval_string[-1] == 'h':
-                return str(f'{int(interval_string[:-1]) * 60 * 60}s')
-
-        except Exception as ex:
-            print(ex)
+        if interval_string[-1] == 's':
+            return interval_string
+        elif interval_string[-1] == 'm':
+            return str(f'{int(interval_string[:-1]) * 60}s')
+        elif interval_string[-1] == 'h':
+            return str(f'{int(interval_string[:-1]) * 60 * 60}s')
